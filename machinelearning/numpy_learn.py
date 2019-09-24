ddfe9174c1b1cad3c65cc9e2efa30699ecfb108a
root <root@localhost.localdomain>
2019-09-24 16:38:29 +0800

    modify numpy_learn.py

--- machinelearning/numpy_learn.py
+++ machinelearning/numpy_learn.py
@@ -1,7 +1,9 @@
 import numpy as np
 import os
 
-print(os.path.abspath("."))
+print(os.path.abspath('.')) #current working directory
+print(os.path.abspath(os.curdir)) #current working directory
+print(os.path.abspath('..')) #current working directory parent directory
 
 array = np.array([1,2,3,4,5])
 print(type(array))
@@ -109,7 +111,7 @@ variance_column = tang_array.var(axis=0)
 variance_row = tang_array.var(axis=1)
 
 ###################################################################################################
-## clip(), round() np.sort() searchsorted(array, values) lexsort()
+## clip(), round() np.sort() linspace() searchsorted(array, values) lexsort() logspace()
 
 arr = np.array([[1,2,3],[4,5,6]])
 #only 2 to 4 data are retained, less than 2 becomes 2 and greater than 4 becomes 4
@@ -145,8 +147,117 @@ index = np.lexsort([-1*arr[:,0], arr[:,2]])
 sort_index = arr[index]
 print(sort_index)
 
+log_arr = np.logspace(0,1,5) # 10 nth power [ 1. 1.77827941  3.16227766  5.62341325 10.]
+print("log_arr",log_arr)
 
 ###################################################################################################
+## transpose()  concatenate(), vstack(), hstack(), flatten()
+
+arr = np.arange(10)
+arr.shape = 2,5
+
+trans_arr = arr.transpose()
+trans_arr = arr.T                 #the same as above line
+
+a = np.array([[1,2,3],[4,5,6]])
+b = np.array([[7,8,9],[10,11,12]])
+
+c = np.concatenate((a,b))         #Vertical stitching matrix 
+c = np.vstack((a,b))              #the same as above line
+
+c = np.concatenate((a,b), axis=1)
+c = np.hstack((a,b))              #the same as above line
+
+c = c.flatten()
+print(c)
+
+
+###################################################################################################
+## zeros()  zeros_like() ones_linke() identity()
+
+zero_arr = np.zeros(3)
+zero_arr = np.zeros((3,3))
+
+one_arr = np.ones((3,3))
+
+zero_arr.fill(1)
+
+np.zeros_like(arr)
+np.ones_like(arr)
+
+diagonal_array = np.identity(5)
+print(diagonal_array)
+
+###################################################################################################
+## multiply()  dot()
+
+x = np.array([5, 5])
+y = np.array([2, 2])
+z = np.multiply(x,y) #[10 10]
+z = np.dot(x,y)      #20 inner product, multiply the corresponding positions and add them together.
+
+x.shape = 2,1
+y.shape = 1,2
+z = np.dot(x,y)       
+"""
+[[10 10]
+ [10 10]]
+"""
+
+###################################################################################################
+## random  shuffle
+
+random_arr = np.random.rand(3,2)
+print(random_arr)
+random_arr = np.random.randint(10,size=(5,4))
+print(random_arr)
+random_arr = np.random.randint(0,10,3)
+print(random_arr)
+
+random = np.random.rand()
+print(random)
+
+random_sample = np.random.random_sample()
+print(random_sample)
+
+mu, sigma = 0, 0.1                        #mean, variance
+np.set_printoptions(precision = 2)        #print tow decimal places
+arr = np.random.normal(mu, sigma, 10)     #random gaussian distribution
+print(arr)
+
+##shuffle
+arr = np.arange(10)
+print(arr)
+arr_shuffle = np.random.shuffle(arr)
+print("shuffle",arr_shuffle)
+
+np.random.seed(100)                     #consistent random results
+mu, sigma = 0, 0.1
+arr = np.random.normal(mu, sigma, 10)
+print(arr)
+
+###################################################################################################
+### savetxt()  loadtxt()
+
+arr = np.array([[1,2,3],[4,5,6]])
+#np.savetxt("arr.txt", arr, fmt="%d")
+#np.savetxt("arr.txt", arr, fmt="%d",delimiter=",")
+np.savetxt("arr.txt", arr, fmt="%.2f",delimiter=",")
+
+#arr.txt: 
+#    1.00,2.00,3.00...
+#    4.00,5.00,6.00...
+#data = np.loadtxt("arr.txt")
+#print(data)
+data = np.loadtxt("arr.txt", delimiter=",")
+print(data)
+#data = np.loadtxt("arr.txt", delimiter=",", skiprows=3)    #skip the first 3 line
+#print(data)
+
+arr = np.array([[1,2,3],[4,5,6]])
+np.save("arr.npy", arr)
+arr = np.load("arr.npy")
+print(arr)
 
 
 
