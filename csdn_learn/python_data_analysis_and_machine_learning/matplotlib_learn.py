af8b8e0491f9cb2026e53ea25212e031996298b3
root <root@localhost.localdomain>
2019-11-04 10:24:28 +0800

    modify matplotlib_learn

---
+++ csdn_learn/python_data_analysis_and_machine_learning/matplotlib_learn.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+###basic picture
+plt.plot([1,2,3,4,5], [1,2,3,4,5]) #paint x axis & y axis
+#plt.show()
+
+plt.xlabel("xlabel", fontsize=16)
+plt.ylabel("ylabel")
+plt.title("Draw", fontsize=20)
+#plt.show()
+
+
+
+
+
