849e08f80e3f3fc73e2ee5d9ba125bbf5b6a97d7
root <root@localhost.localdomain>
2019-11-06 06:27:47 +0800

    modify plot chart.py

--- csdn_learn/python_data_analysis_and_machine_learning/matplotlib_learn/1_plot_chart.py
+++ csdn_learn/python_data_analysis_and_machine_learning/matplotlib_learn/1_plot_chart.py
@@ -56,6 +56,9 @@ plt.subplot(326)
 plt.plot(x, y, color="r")
 plt.show()
 
+
+
+"""采用多种style use a variety of styles
 x = np.linspace(-10, 10)
 y = np.sin(x)
 #plt.style.use("dark_background")
@@ -68,4 +71,97 @@ plt.show()
 plt.xkcd()
 plt.plot(x, y)
 plt.show()
+"""
+
+
+
+"""消除x轴y轴坐标eliminate x-axis yaxis coordinates
+x = range(10)
+y = range(10)
+#当前的图表和子图可以使用plt.gcf()和plt.gca()获得，分别表示Get Current Figure和Get Current Axes。
+#在pyplot模块中，许多函数都是对当前的Figure或Axes对象进行处理，
+#比如说：plt.plot()实际上会通过plt.gca()获得当前的Axes对象ax，然后再调用ax.plot()方法实现真正的绘图。
+ax = plt.gca()
+plt.plot(x, y)
+ax.axes.get_xaxis().set_visible(False)
+ax.axes.get_yaxis().set_visible(False)
+plt.show()
+"""
+
+
+"""消除x轴y轴坐标eliminate x-axis yaxis coordinates
+import math
+x = np.random.normal(loc=0.0, scale=1.0, size=300)
+width = 0.5
+bins = np.arange(math.floor(x.min()) - width, math.ceil(x.max()) + width, width)
+
+ax = plt.subplot(111)
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+
+plt.tick_params(bottom="off", top="off", left="off", right="off")
+
+plt.hist(x, alpha=0.5, bins=bins)
+
+plt.grid()
+
+plt.show()
+"""
+
+
+"""添加x坐标信息，并旋转右对齐 add x-axis coordinate information and rotate right alignment
+x = range(10)
+y = range(10)
+labels = ["information" for i in range(10)]
+fig, ax = plt.subplots()
+plt.plot(x, y)
+
+#x轴坐标信息右对齐x-axis coordinate information is right aligned
+ax.set_xticklabels(labels, rotation=45, horizontalalignment="right")
+
+plt.show()
+"""
+
+x = np.arange(10)
+for i in range(1, 4):
+    #label="":当前线的所属类别的信息information about the category of the current line
+    plt.plot(x, i*x**2, label="Group %d" %i, marker="o")
+#在图中加入上面的label标识add the above label to the figure
+#framealpha:透明度 transparency 
+#plt.legend(loc="best", framealpha=0.1)
+plt.legend(loc="upper right", framealpha=0.01)
+#plt.legend(loc="upper left") 
+plt.show()
+
+
+###设置全局变脸参数，使所有图的title fontsize=30
+import matplotlib as mpl
+mpl.rcParams["axes.titlesize"] = "30"
+
+fig = plt.figure()
+ax = plt.subplot(111)
+x = np.arange(10)
+for i in range(1, 4):
+    plt.plot(x, i*x**2, label="Group %d"%i)
+#在图外加入上面的label标识 add the above label to the outside of the figure
+#bbox_to_anchor:labels 存放位置，参数需要根据自己需要调节,labels storage location, parameters need to be adjust
+#ncol: labels 分成三列来放置 labels are placed in three columns
+#framealpha:透明度 transparency 
+ax.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, framealpha=0.01)
+#ax.legend(loc="upper center", bbox_to_anchor=(1.07, 1), ncol=1, framealpha=0.1)
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.set_title("title information") # 不指定fontsize的话会默认为上面的全局参数fontsize=30
+ax.set_title("title information", fontsize=20)
+plt.show()
+
+
+
+
+
+
+
+
+
+
 
