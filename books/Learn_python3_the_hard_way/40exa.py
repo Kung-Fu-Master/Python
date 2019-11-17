b6e057b394e3083dda788b363319e915c827d7ca
Kung-Fu-Master <860176063@qq.com>
2019-11-17 17:14:35 +0800

    add learn python3 the hard way

---
+++ books/Learn_python3_the_hard_way/40exa.py
@@ -0,0 +1,9 @@
+class MyStuff(object):
+    def __init__(self):
+        self.targerine = "And now a thousand years between"
+    def apple(self):
+        print("I AM CLASS APPLES!")
+
+thing = MyStuff()
+thing.apple()
+print(thing.targerine)
